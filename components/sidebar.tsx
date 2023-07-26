46fe342061d06289cd3c42e877c0758fbe00de82
dadsec-dev <geecypher41@gmail.com>
2023-07-26 21:04:13 +0100

    finished dahsboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import React from 'react'
 import Image from 'next/image';
 import { Montserrat } from 'next/font/google';
+import {usePathname} from 'next/navigation'
 
 import { cn } from '@/lib/utils';
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react';
@@ -49,6 +50,7 @@ const routes = [{
 }]
 
 const Sidebar = () => {
+    const pathname = usePathname();
   return (
     <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
             <div className='px-3 py-2 flex-1'>
@@ -64,7 +66,7 @@ const Sidebar = () => {
 
                 <div className='space-y-1'>
                     {routes.map((route) => (
-                        <Link href={route.href} key={route.href} className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'>
+                        <Link href={route.href} key={route.href} className={cn('text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition', pathname === route.href ? 'text-white bg-white/10 ' : 'text-zinc-400')}>
                             <div className='flex items-center flex-1'>
                                     <route.icon className={cn("h-5 w-5 mr-3", route.color)}/>
 
